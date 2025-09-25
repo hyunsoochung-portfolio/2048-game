44b154df1d1dc9a30d3b456628b8d05e63e8e39f
hyunsoo <hyunsoo3318@snu.ac.kr>
2025-09-26 00:32:37 +0900

    style: simplify import comments in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css'; // App 전역 스타일을 사용할 경우 유지
-import Game from './components/Game'; // Game 컴포넌트 불러오기
+import './App.css'; 
+import Game from './components/Game'; 
 
 function App() {
   return (
